175ab0169065f5c9ff68d6e8014d5e5f9913f4a8
Greenwood <greenwood672.dev@gmail.com>
2026-01-05 00:20:19 +0000

    Added continuous comms

--- Project/controller/marl/env_wrapper.py
+++ Project/controller/marl/env_wrapper.py
@@ -1,9 +1,12 @@
+import numpy as np
+import torch
 
 
 class SimWrapper:
-    def __init__(self, sim):
+    def __init__(self, sim, comm_dim=5):
         self._sim = sim
         self._num_agents = sim.config.n_agents
+        self._comm_dim = comm_dim
 
     def parallel_step(self, args):
         world_id, action = args
@@ -18,17 +21,27 @@ class SimWrapper:
     def get_agent_action_count(self, world_id):
         return self._sim.get_agent_action_count(world_id)
 
-    def get_agent_obs(self, world_id, agent_id):
-        return self._sim.get_agent_obs(world_id, agent_id)
+    def get_agent_obs(self, world_id, agent_id, world_comms=None):
+        if world_comms is None:
+            world_comms = np.zeros(self.get_world_comms_size())
+        else:
+            if isinstance(world_comms, torch.Tensor):
+                world_comms = world_comms.detach().cpu().numpy()
+            world_comms = world_comms.flatten()
+
+        return np.concatenate([self._sim.get_agent_obs(world_id, agent_id), world_comms])
+    
+    def get_world_comms_size(self):
+        return self._num_agents * self._comm_dim
     
     def get_agent_obs_size(self, world_id):
-        return self._sim.get_agent_obs_size(world_id)
+        return self._sim.get_agent_obs_size(world_id) + self.get_world_comms_size()
 
     def get_agent_reward(self, world_id, agent_id):
         return self._sim.get_agent_reward(world_id, agent_id)
     
-    def get_agents_obs(self, world_id):
-        return [self.get_agent_obs(world_id, agent_id) for agent_id in range(self._num_agents)]
+    def get_agents_obs(self, world_id, world_comms=None):
+        return [self.get_agent_obs(world_id, agent_id, world_comms) for agent_id in range(self._num_agents)]
     
     def get_agents_reward(self, world_id):
         return [self.get_agent_reward(world_id, agent_id) for agent_id in range(self._num_agents)]
--- Project/todo.md
+++ Project/todo.md
@@ -2,12 +2,6 @@
 Chunk updating model - not necassarily needed
 Better logs
 tests
-load configs properly
+
 
 communication head
-
-
-
-Current:
-c_values is [batchsize, timesteps]
-but needs [batchsize, timesteps, agents] for compute_advantages
