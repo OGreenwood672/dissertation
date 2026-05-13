a556a002ca60411a54db6dd70735befa686e45b2
Greenwood <greenwood672.dev@gmail.com>
2026-05-13 02:15:23 +0100

    Added alastair feedback

--- Project/notebooks/evaluation/vectorised_timings.py
+++ Project/notebooks/evaluation/vectorised_timings.py
@@ -38,7 +38,7 @@ GO = 95
 NC = 1
 C = 8
 
-WORLDS = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+WORLDS = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384]
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -46,25 +46,24 @@ def simulate_python_simulation(world_idx):
     agent_pos = np.random.rand(N, 2)
     station_pos = np.random.rand(5, 2)
     
-    for _ in range(T):
-        for i in range(N):
-            closest_dist = float('inf')
-            closest_station = -1
-            
-            for j in range(5):
-                dist = math.hypot(agent_pos[i][0] - station_pos[j][0], 
-                                  agent_pos[i][1] - station_pos[j][1])
-                if dist < closest_dist:
-                    closest_dist = dist
-                    closest_station = j
-                    
-            agent_pos[i][0] += 0.1
-            agent_pos[i][1] += 0.1
-            
-        fake_obs = np.random.rand(N, O).astype(np.float32)
-        fake_global = np.random.rand(GO).astype(np.float32)
-        fake_targets = np.random.rand(N, 3).astype(np.float32)
-        fake_rewards = np.random.rand(N).astype(np.float32)
+    for i in range(N):
+        closest_dist = float('inf')
+        closest_station = -1
+        
+        for j in range(5):
+            dist = math.hypot(agent_pos[i][0] - station_pos[j][0], 
+                                agent_pos[i][1] - station_pos[j][1])
+            if dist < closest_dist:
+                closest_dist = dist
+                closest_station = j
+                
+        agent_pos[i][0] += 0.1
+        agent_pos[i][1] += 0.1
+        
+    fake_obs = np.random.rand(N, O).astype(np.float32)
+    fake_global = np.random.rand(GO).astype(np.float32)
+    fake_targets = np.random.rand(N, 3).astype(np.float32)
+    fake_rewards = np.random.rand(N).astype(np.float32)
 
     return fake_obs, fake_global, fake_targets, fake_rewards
 
@@ -72,67 +71,82 @@ if __name__ == "__main__":
 
     print(f"Using device: {device}")
 
-    rust_worlds_parallised_to_sps = []
+    RUNS = 10
+
+    results = []
 
     for W in tqdm(WORLDS):
         config.training.worlds_parallised = W
-        system, config = setup(config, device) 
-        sim = system['sim']
-        
-        dummy_actions = np.zeros((W, N), dtype=np.int32)
 
-        start = perf_counter()
-        
-        for t in range(T):
-            curr_obs, curr_global_obs, curr_targets, rewards = sim.parallel_step(dummy_actions)
+        for run in range(RUNS):
+
+            system, config = setup(config, device) 
+            sim = system['sim']
             
-        runtime = perf_counter() - start
-        sps = (T * W) / runtime
-        
-        rust_worlds_parallised_to_sps.append([W, sps])
-        
-        sim.shutdown()
-        del system
+            dummy_actions = np.zeros((W, N), dtype=np.int32)
 
-    python_worlds_parallised_to_sps = []
+            start = perf_counter()
+            
+            for t in range(T):
+                curr_obs, curr_global_obs, curr_targets, rewards = sim.parallel_step(dummy_actions)
+                
+            runtime = perf_counter() - start
+            sps = (T * W) / runtime
+            
+            results.append({"Worlds Parallelised": W, "SPS": sps, "Language": "Rust"})
+            
+            sim.shutdown()
+            del system
 
     with Pool() as pool:
         for W in tqdm(WORLDS):
-            start = perf_counter()
-            for t in tqdm(range(T), leave=False):
-                if W == 1:
-                    results = simulate_python_simulation(0)
-                else:
-                    results = pool.map(simulate_python_simulation, range(W))
-            runtime = perf_counter() - start
-            sps = (T * W) / runtime
-            python_worlds_parallised_to_sps.append([W, sps])
+            for run in range(RUNS):
+                start = perf_counter()
+                for t in tqdm(range(T), leave=False):
+                    if W == 1:
+                        data = simulate_python_simulation(0)
+                    else:
+                        data = pool.map(simulate_python_simulation, range(W))
+                runtime = perf_counter() - start
+                sps = (T * W) / runtime
+                results.append({"Worlds Parallelised": W, "SPS": sps, "Language": "Python"})
     
-    worlds_parallised_to_sps = [[i[0], i[1], j[1]] for i, j in zip(python_worlds_parallised_to_sps, rust_worlds_parallised_to_sps)]
+    # worlds_parallised_to_sps = [[i[0], i[1], j[1]] for i, j in zip(python_worlds_parallised_to_sps, rust_worlds_parallised_to_sps)]
 
-    df = pd.DataFrame(worlds_parallised_to_sps, columns=["worlds_parallelised", "python_steps_per_second", "rust_steps_per_second"])
+    # df = pd.DataFrame(worlds_parallised_to_sps, columns=["worlds_parallelised", "python_steps_per_second", "rust_steps_per_second"])
+
+    df = pd.DataFrame(results)
 
     plt.figure(figsize=(8, 5))
 
+    # sns.lineplot(
+    #     data=df,
+    #     x="worlds_parallelised",
+    #     y="python_steps_per_second",
+    #     marker="o",
+    #     label="Python"
+    # )
+    # sns.lineplot(
+    #     data=df,
+    #     x="worlds_parallelised",
+    #     y="rust_steps_per_second",
+    #     marker="o",
+    #     label="Rust"
+    # )
+
     sns.lineplot(
         data=df,
-        x="worlds_parallelised",
-        y="python_steps_per_second",
+        x="Worlds Parallelised",
+        y="SPS",
+        hue="Language",
         marker="o",
-        label="Python"
-    )
-    sns.lineplot(
-        data=df,
-        x="worlds_parallelised",
-        y="rust_steps_per_second",
-        marker="o",
-        label="Rust"
+        errorbar="sd",
     )
 
     plt.yscale("log", base=10)
-    plt.xlabel("Worlds parallelised")
+    plt.xlabel("Worlds Parallelised")
     plt.ylabel("Steps per Second (SPS) (log scale)")
-    plt.title("SPS vs Worlds parallelised")
+    plt.title("SPS vs Worlds Parallelised")
 
     optimal_worlds = 960
     optimal_sps = 240000
@@ -147,44 +161,52 @@ if __name__ == "__main__":
 
     plt.text(
         optimal_worlds,
-        45,
-        "Optimal = 960 worlds",
+        0.02,
+        f"Optimal = {optimal_worlds} worlds",
         rotation=90,
         va="bottom",
         ha="right",
         fontsize=8,
-        color="black"
+        color="black",
+        transform=plt.gca().get_xaxis_transform()
     )
 
-    python_idx = df["python_steps_per_second"].idxmax()
-    python_x = df.loc[python_idx, "worlds_parallelised"]
-    python_y = df.loc[python_idx, "python_steps_per_second"]
+    mean_df = df.groupby(["Language", "Worlds Parallelised"]).mean().reset_index()
 
-    rust_idx = df["rust_steps_per_second"].idxmax()
-    rust_x = df.loc[rust_idx, "worlds_parallelised"]
-    rust_y = df.loc[rust_idx, "rust_steps_per_second"]
+    python_df = mean_df[mean_df["Language"] == "Python"].reset_index(drop=True)
+    python_idx = python_df["SPS"].idxmax()
+    python_x = python_df.loc[python_idx, "Worlds Parallelised"]
+    python_y = python_df.loc[python_idx, "SPS"]
+
+    rust_df = mean_df[mean_df["Language"] == "Rust"].reset_index(drop=True)
+    rust_idx = rust_df["SPS"].idxmax()
+    rust_x = rust_df.loc[rust_idx, "Worlds Parallelised"]
+    rust_y = rust_df.loc[rust_idx, "SPS"]
 
     plt.annotate(
         f"Python peak ≈ {python_y:,.0f} SPS",
         xy=(python_x, python_y),
-        xytext=(python_x + 40, python_y * 0.7),
+        xytext=(python_x, python_y * 0.7),
         textcoords="data",
         arrowprops=dict(arrowstyle="->", lw=1),
-        fontsize=8
+        fontsize=8,
+        ha="right"
     )
 
     plt.annotate(
         f"Rust peak ≈ {rust_y:,.0f} SPS",
         xy=(rust_x, rust_y),
-        xytext=(rust_x - 220, rust_y / 1.6),
+        xytext=(rust_x, rust_y / 1.6),
         textcoords="data",
         arrowprops=dict(arrowstyle="->", lw=1),
-        fontsize=8
+        fontsize=8,
+        ha="right"
     )
 
     plt.grid(True, alpha=0.3, which="both")
     plt.tight_layout()
     
-    plt.savefig(FIGURES_DIR / "sim-comparison.png", dpi=600, bbox_inches='tight', facecolor='white')
+    # plt.savefig(FIGURES_DIR / "sim-comparison.png", dpi=600, bbox_inches='tight', facecolor='white')
+    # plt.savefig("C:/Users/green/Downloads/sim-comparison.png", dpi=600, bbox_inches='tight', facecolor='white')
     # plt.show()
     plt.close('all')
